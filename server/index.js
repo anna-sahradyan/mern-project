a8612744d38a459966c8cd8d68078a125c867ac3
anna <a.sahradyan72@gmail.com>
2022-09-23 11:26:55 +0400

    start mern project

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        app.listen(5000, () => {
+            console.log(`Server is running at http://localhost:${PORT} `);
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+start();
